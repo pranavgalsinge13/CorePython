813242d772100c3ce21a9fedd5432e3322c8a23a
pranavgalsinge13 <pranavgalsinge13@gmail.com>
2026-08-31 15:33:49 +0530

    Update array.py
    concatenation in array

--- array.py
+++ array.py
@@ -31,3 +31,13 @@ import array as arr
 num=arr.array('i',[1,2,3,4,5])
 del num[2]
 print(num)
+
+# concatenation
+
+import array as arr
+a=arr.array('d',[1.0,2.0,3.0,4.0,5.0])
+b=arr.array('d',[2.1,3.1])
+c=arr.array('d')
+
+c=a+b
+print("C=",c)
