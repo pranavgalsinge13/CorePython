f447fa717d234e3edcd529915d2da7eff09c0d3f
pranavgalsinge13 <pranavgalsinge13@gmail.com>
2026-09-10 15:55:07 +0530

    Update OOPs.py

--- OOPs.py
+++ OOPs.py
@@ -1,6 +1,6 @@
 # oops in python
 
-class student:
+'''class student:
     def __init__(self):
         self.name='pranav'
         self.age=20
@@ -13,4 +13,22 @@ class student:
 
 
 s1=student()
+s1.display()'''
+
+
+class student:
+    def __init__(self,name,age,marks):
+        self.name=name
+        self.age=age
+        self.marks=marks
+
+    def display(self):
+        print("Name of the student is:",self.name)
+        print("Age of the student is:",self.age)
+        print("Marks of the student is:",self.marks)
+
+
+s1=student("Pranav",20,90)
+s2=student("Yash",23,95)
 s1.display()
+s2.display()
